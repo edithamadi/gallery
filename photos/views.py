804ed5be33cbc399a92e8923ddc1f96e9d3295ea
Edith <you@example.com>
2018-09-26 19:44:39 +0300

    created a view

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http  import HttpResponse
+import datetime as dt
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the photo gallery')
+
+def photos_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
