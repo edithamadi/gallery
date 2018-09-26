2e9548ada8d8fed09c189386a7d42d6d99fb0e3f
Edith <you@example.com>
2018-09-26 20:27:06 +0300

    created first template

--- photos/views.py
+++ photos/views.py
@@ -3,7 +3,7 @@ from django.http  import HttpResponse,Http404
 import datetime as dt
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the photo gallery')
+    return render(request, 'welcome.html')
 
 def photos_of_day(request):
     date = dt.date.today()
