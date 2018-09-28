0e21efdc92b01a387b4bae1730a500ecb2320c53
Edith <you@example.com>
2018-09-28 12:13:30 +0300

    created models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
