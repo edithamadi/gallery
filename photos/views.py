b35429bb7841a902fa1687bf6c789371cd4f878b
Edith <you@example.com>
2018-09-28 21:04:11 +0300

    displayed image

--- photos/views.py
+++ photos/views.py
@@ -1,38 +1,12 @@
 from django.shortcuts import render,redirect
+from .models import Image
 from django.http  import HttpResponse,Http404
 import datetime as dt
 # Create your views here.
 def welcome(request):
     return render(request, 'all-photos/today-photos.html')
 
-# def photos_of_day(request):
-#     date = dt.date.today()
-
-#     return render(request, 'all-photos/today-photos.html',{"date": date,})
-
-# def convert_dates(dates):
-
-#     # Function that gets the weekday number for the date.
-#     day_number = dt.date.weekday(dates)
-
-#     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-#     # Returning the actual day of the week
-#     day = days[day_number]
-#     return day
-
-# def past_days_photos(request,past_date):
-#     try:
-#         # Converts data from the string Url
-#         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-#     except ValueError:
-#             #Raise 404 error when ValueError is thrown raise Http404()
-#         raise Http404()
-#         assert False
-
-#     if date == dt.date.today():
-#         return redirect(photos_of_day)
-
+def photos(request):
+    photos = Image.objects.all()
     
-#     return render(request, 'all-photos/past-photos.html',{"date": date})
+    return render(request, 'all-photos/today-photos.html', {"photos":photos})
