4232733ab0b8df4081941958d65a4efda187e4aa
Edith <you@example.com>
2018-09-28 19:15:12 +0300

    editted model fields

--- photos/admin.py
+++ photos/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Location,Image,category
 # Register your models here.
 
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('category',)
 
 admin.site.register(Location)
 admin.site.register(Image)
