afc5ce776f82ad844dd77f10496e362840a55c9a
Edith <you@example.com>
2018-09-29 15:08:56 +0300

    created special route to display image

--- photos/urls.py
+++ photos/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.photos,name = 'photos'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+     url(r'^image/(\d+)',views.image,name ='image'
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
